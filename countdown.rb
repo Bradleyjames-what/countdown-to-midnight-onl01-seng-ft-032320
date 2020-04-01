aa45f6953a3284311a6df9e39048f2861a79767e
Bradley Rush <bradleyj.rush@gmail.com>
2020-04-01 16:56:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
-
 def countdown(number)
- while number > 0
-   puts "#{number} SECOND(S)!"
-   number -= 1
-  if number == 0 || number > 10
-    return "HAPPY NEW YEAR!"
- end
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    if number == 0 || number > 10
+      return "HAPPY NEW YEAR!"
+    end
+  end
 end
  
   
