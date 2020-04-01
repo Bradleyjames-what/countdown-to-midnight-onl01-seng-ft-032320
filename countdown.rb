1dbb0022f2c8b7a41166f224c79a5c736198cb67
Bradley Rush <bradleyj.rush@gmail.com>
2020-04-01 16:51:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 
 def countdown(number)
  while number > 0
-   $stdout.puts "#{number} SECOND(S)!"
+   puts "#{number} SECOND(S)!"
    number -= 1
   if number == 0 || number > 10
     return "HAPPY NEW YEAR!"
